1edba3f8db95a9cbc89e8d4cc3f327469ed75646
jvyden <jvyden@jvyden.xyz>
2025-05-09 18:41:56 -0400

    brotli asset reading

--- NeoFrost/Patches/DataTreeConverterPatches.cs
+++ NeoFrost/Patches/DataTreeConverterPatches.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.IO.Compression;
 using System.Reflection;
+using System.Text;
 using BaseX;
-using BrotliSharpLib;
+using Brotli;
 using HarmonyLib;
 using Newtonsoft.Json.Bson;
 
@@ -15,7 +15,7 @@ namespace NeoFrost.Patches;
 public static class DataTreeConverterPatches
 {
     [HarmonyPatch(typeof(DataTreeConverter), "Load", typeof(string), typeof(string))]
-    // [HarmonyPrefix]
+    [HarmonyPrefix]
     public static bool LoadPrefix(string file, string ext, ref DataTreeDictionary __result)
     {
         // ReSharper disable once RedundantAssignment
@@ -26,26 +26,57 @@ public static class DataTreeConverterPatches
             return true;
 
         using FileStream fs = File.OpenRead(file);
+        bool success = TryReadHeader(fs, out int version, out ulong compression);
+        if(success)
+            UniLog.Log($"Reading brotli asset: version={version},compression={compression}");
+
         __result = FromBrotli(fs);
         return false;
     }
 
     private static readonly MethodInfo DataTreeConverter_Read = AccessTools.Method(typeof(DataTreeConverter), "Read");
+
+    private static void DecompressFromBrotli(Stream inStream, Stream destStream)
+    {
+        using BrotliStream brotliStream = new(inStream, CompressionMode.Decompress, true);
+        brotliStream.CopyTo(destStream);
+        destStream.Flush();
+    }
+    
+    private static bool TryReadHeader(Stream stream, out int version, out ulong compression)
+    {
+        using BinaryReader binaryReader = new(stream, Encoding.UTF8, true);
+        for (int i = 0; i < "FrDT".Length; i++)
+        {
+            if (binaryReader.ReadByte() == (byte)"FrDT"[i]) continue;
+
+            version = -1;
+            compression = 0;
+            return false;
+        }
+        
+        version = binaryReader.ReadInt32();
+        compression = binaryReader.Read7BitEncoded();
+        
+        return true;
+    }
+    
     private static DataTreeDictionary FromBrotli(FileStream stream)
     {
-        using BrotliStream brotliStream = new(stream, CompressionMode.Decompress, true);
-        using BsonDataReader bsonDataReader = new(brotliStream);
+        UniLog.Log("MAKING BROTLI STREAM");
+        
+        using MemoryStream ms = new();
+        DecompressFromBrotli(stream, ms);
+        ms.Seek(0, SeekOrigin.Begin);
+        
+        UniLog.Log("MAKING BSON READER");
+        
+        using BsonDataReader bsonDataReader = new(ms);
+        bsonDataReader.CloseInput = false;
 
-        try
-        {
-            DataTreeDictionary dataTreeDictionary =
-                (DataTreeDictionary)DataTreeConverter_Read.Invoke(null, [bsonDataReader]);
-            return dataTreeDictionary;
-        }
-        catch (Exception e)
-        {
-            UniLog.Error(e.ToString(), false);
-            return null!;
-        }
+        UniLog.Log("DECOMPRESSING BROTLI STREAM");
+        DataTreeDictionary dataTreeDictionary = (DataTreeDictionary)DataTreeConverter_Read.Invoke(null, [bsonDataReader]);
+        UniLog.Log("DECOMPRESSED BROTLI STREAM");
+        return dataTreeDictionary;
     }
 }
